2030e66bd4cf3ef5e9ade4b9556b4b7bf385e6ee
ShariMaes <r0260793@student.thomasmore.be>
2015-12-21 11:22:30 +0100

    Added methods

--- Readable/MainWindow.xaml.cs
+++ Readable/MainWindow.xaml.cs
@@ -22,49 +22,72 @@ namespace Readable
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
         public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+
+           // string cmdstring = @"CREATE TABLE books (Id INTEGER NOT NULL, Title TEXT, Description TEXT, Pages INTEGER, PRIMARY KEY (Id))";
+           // string cmdstring = @"INSERT INTO books (Title, Description, Pages) VALUES ('Test book', 'Description test', 200);";
+           // SQLiteCommand mycommand = new SQLiteCommand(cmdstring, cnn);
+           // mycommand.ExecuteNonQuery();        
+        }
+
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source=ReadableDB.s3db;Version=3;New=True;");
+        }
+
+        void LoadData()
         {
             try
             {
-            InitializeComponent();
+                SetConnection();
+                sql_con.Open();
+                sql_cmd = sql_con.CreateCommand();
+                string CommandText = "SELECT Title FROM books";
+                DB = new SQLiteDataAdapter(CommandText, sql_con);
+                DS.Reset();
+                DB.Fill(DS);
+                DT = DS.Tables[0];
 
-            SQLiteConnection cnn = new SQLiteConnection("Data Source=ReadableDB.s3db;Version=3;New=True;");
-            cnn.Open();
-
-           // string cmdstring = @"CREATE TABLE books (Id INTEGER NOT NULL, Title TEXT, Description TEXT, Pages INTEGER, PRIMARY KEY (Id))";
-
-           // string cmdstring = @"INSERT INTO books (Title, Description, Pages) VALUES ('Test book', 'Description test', 200);";
-
-           // SQLiteCommand mycommand = new SQLiteCommand(cmdstring, cnn);
-           // mycommand.ExecuteNonQuery();
-
-            SQLiteDataAdapter DB;
-            DataSet DS = new DataSet();
-            DataTable dt = new DataTable();
-
-            DB = new SQLiteDataAdapter("SELECT Title FROM books", cnn);
-            DS.Reset();
-            DB.Fill(DS);
-            dt = DS.Tables[0];
-
-            DataTable dtable = new DataTable();
-            MainListView.Items.Clear();
-            for (int i = 0; i < dt.Rows.Count; i++)
+                DataTable dtable = new DataTable();
+                MainListView.Items.Clear();
+                for (int i = 0; i < DT.Rows.Count; i++)
+                {
+                    DataRow drow = DT.Rows[i];
+                    ListViewItem lvi = new ListViewItem { Content = drow.ToString() };
+                    MainListView.Items.Add(new ListViewItem { Content = drow["Title"].ToString() });
+                }
+                sql_con.Close();
+            }catch(Exception ex)
             {
-                DataRow drow = dt.Rows[i];
-                ListViewItem lvi = new ListViewItem { Content = drow.ToString()};
-                MainListView.Items.Add(new ListViewItem { Content = drow["Title"].ToString() });
-            }
-          
-            cnn.Close();
-            }
-            catch(Exception ex)
-            {
-                System.Windows.MessageBox.Show(ex.Message.ToString());
+                System.Windows.MessageBox.Show(ex.ToString());
             }
 
         }
 
+        //private void ExecuteQuery(string txtQuery)
+        //{
+        //    SetConnection();
+        //    sql_con.Open();
+        //    sql_cmd = sql_con.CreateCommand();
+        //    sql_cmd.CommandText = txtQuery;
+        //    sql_cmd.ExecuteNonQuery();
+        //    sql_con.Close();
+        //}
+
+        //private void Add()
+        //{
+        //    string txtSQLQuery = "insert into  mains (desc) values ('" + txtDesc.Text + "')";
+        //    ExecuteQuery(txtSQLQuery);
+        //}
 
     }
 }
