a525df6f791afe43074c8b69993554272c740cf4
ShariMaes <r0260793@student.thomasmore.be>
2015-12-22 16:25:37 +0100

    Added radiobuttons for owned and GenreList

--- Readable/MainWindow.xaml.cs
+++ Readable/MainWindow.xaml.cs
@@ -28,16 +28,17 @@ namespace Readable
         private DataSet DS = new DataSet();
         private DataTable DT = new DataTable();
 
+        private SQLiteDataAdapter DBG;
+        private DataSet DSG = new DataSet();
+        private DataTable DTG = new DataTable();
+
         public MainWindow()
         {
             InitializeComponent();
             LoadDataListView();
-            Radiobuttons();
-
-           // string cmdstring = @"CREATE TABLE books (Id INTEGER NOT NULL, Title TEXT, Description TEXT, Pages INTEGER, PRIMARY KEY (Id))";
-           // string cmdstring = @"INSERT INTO books (Title, Description, Pages) VALUES ('Test book', 'Description test', 200);";
-           // SQLiteCommand mycommand = new SQLiteCommand(cmdstring, cnn);
-           // mycommand.ExecuteNonQuery();        
+            LoadDataGenreListView();
+            RadiobuttonsRead();
+            RadiobuttonsOwned();       
         }
 
         private void SetConnection()
@@ -52,7 +53,7 @@ namespace Readable
                 SetConnection();
                 sql_con.Open();
                 sql_cmd = sql_con.CreateCommand();
-                string CommandText = "SELECT b.Title,b.State, a.Name FROM books b LEFT JOIN author a ON b.authorId=a.Id"; //WHERE b.authorId = a.Id"     
+                string CommandText = "SELECT b.*, a.*, bg.* , g.* FROM books b LEFT JOIN author a ON b.authorId = a.Id LEFT JOIN BookGenre bg on b.GenreId = bg.BookId LEFT JOIN Genre g on bg.GenreId = g.Id";  
                 DB = new SQLiteDataAdapter(CommandText, sql_con);
                 DS.Reset();
                 DB.Fill(DS);
@@ -73,7 +74,36 @@ namespace Readable
             }
         }
 
-        private void Radiobuttons()
+        private void LoadDataGenreListView()
+        {
+            try
+            {
+                SetConnection();
+                sql_con.Open();
+                sql_cmd = sql_con.CreateCommand();
+                string CommandText = "SELECT Genre FROM Genre";
+                DBG = new SQLiteDataAdapter(CommandText, sql_con);
+                DSG.Reset();
+                DBG.Fill(DSG);
+                DTG = DSG.Tables[0];
+
+                DataTable dtable = new DataTable();
+                GenreListView.Items.Clear();
+                for (int i = 0; i < DTG.Rows.Count; i++)
+                {
+                    DataRow drow = DTG.Rows[i];
+                    ListViewItem lvi = new ListViewItem { Content = drow.ToString() };
+                    GenreListView.Items.Add(new ListViewItem { Content = drow["Genre"].ToString() });
+                }             
+                sql_con.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void RadiobuttonsRead()
         {
             try
             {
@@ -142,6 +172,165 @@ namespace Readable
                 MainListView.Items.Add(drarray[i]["Title"].ToString() + " - " + drarray[i]["Name"].ToString());
             }
         }
+
+        private void RadiobuttonsOwned()
+        {
+            try
+            {
+                SetConnection();
+                sql_con.Open();
+                sql_cmd = sql_con.CreateCommand();
+                sql_cmd.CommandText = "SELECT count(Owned) FROM books WHERE Owned = 'In bezit'";
+                sql_cmd.ExecuteNonQuery();
+                Int32 countOwned = Convert.ToInt32(sql_cmd.ExecuteScalar());
+
+                sql_cmd.CommandText = "SELECT count(Owned) FROM books WHERE Owned = 'Niet in bezit'";
+                sql_cmd.ExecuteNonQuery();
+                Int32 countNotOwned = Convert.ToInt32(sql_cmd.ExecuteScalar());
+
+                sql_cmd.CommandText = "SELECT count(Id) FROM books";
+                sql_cmd.ExecuteNonQuery();
+                Int32 countAll = Convert.ToInt32(sql_cmd.ExecuteScalar());
+
+                RadioButtonAllOwned.Content = "Alles (" + countAll + ")";
+                RadioButtonOwned.Content = "In bezit (" + countOwned + ")";
+                RadioButtonNotOwned.Content = "Niet in bezit (" + countNotOwned + ")";
+
+                sql_con.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void RadioButtonAllOwned_Checked(object sender, RoutedEventArgs e)
+        {
+            string filterExp = "Owned = 'In bezit' OR Owned = 'Niet in bezit' OR Owned IS NULL OR Owned = ''";
+            string sortExp = "Title";
+            DataRow[] drarray;
+            drarray = DT.Select(filterExp, sortExp, DataViewRowState.CurrentRows);
+            MainListView.Items.Clear();
+            for (int i = 0; i < drarray.Length; i++)
+            {
+                MainListView.Items.Add(drarray[i]["Title"].ToString() + " - " + drarray[i]["Name"].ToString());
+            }
+        }
+
+        private void RadioButtonOwned_Checked(object sender, RoutedEventArgs e)
+        {
+            string filterExp = "Owned = 'In bezit'";
+            string sortExp = "Title";
+            DataRow[] drarray;
+            drarray = DT.Select(filterExp, sortExp, DataViewRowState.CurrentRows);
+            MainListView.Items.Clear();
+            for (int i = 0; i < drarray.Length; i++)
+            {
+                MainListView.Items.Add(drarray[i]["Title"].ToString() + " - " + drarray[i]["Name"].ToString());
+            }
+        }
+
+        private void RadioButtonNotOwned_Checked(object sender, RoutedEventArgs e)
+        {
+            string filterExp = "Owned = 'Niet in bezit'";
+            string sortExp = "Title";
+            DataRow[] drarray;
+            drarray = DT.Select(filterExp, sortExp, DataViewRowState.CurrentRows);
+            MainListView.Items.Clear();
+            for (int i = 0; i < drarray.Length; i++)
+            {
+                MainListView.Items.Add(drarray[i]["Title"].ToString() + " - " + drarray[i]["Name"].ToString());
+            }
+        }
+
+        //TODO: Publisher toevoegen aan combobox en GenreListView
+        private void ComboGenreList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try{
+             if (((ComboBoxItem)ComboGenreList.SelectedItem).Content.ToString().Equals("Genre"))
+                {
+                    SetConnection();
+                    sql_con.Open();
+                    sql_cmd = sql_con.CreateCommand();
+                    string CommandText = "SELECT Genre FROM Genre";
+                    DBG = new SQLiteDataAdapter(CommandText, sql_con);
+                    DSG.Reset();
+                    DBG.Fill(DSG);
+                    DTG = DSG.Tables[0];
+
+                    DataTable dtable = new DataTable();
+                    GenreListView.Items.Clear();
+                    for (int i = 0; i < DTG.Rows.Count; i++)
+                    {
+                        DataRow drow = DTG.Rows[i];
+                        ListViewItem lvi = new ListViewItem { Content = drow.ToString() };
+                        GenreListView.Items.Add(new ListViewItem { Content = drow["Genre"].ToString() });
+                    }
+                }
+                else if (((ComboBoxItem)ComboGenreList.SelectedItem).Content.ToString().Equals("Taal"))
+                {
+                    SetConnection();
+                    sql_con.Open();
+                    sql_cmd = sql_con.CreateCommand();
+                    string CommandText = "SELECT Language FROM books";
+                    DBG = new SQLiteDataAdapter(CommandText, sql_con);
+                    DSG.Reset();
+                    DBG.Fill(DSG);
+                    DTG = DSG.Tables[0];
+
+                    //TODO: Counts toevoegen aan Genre en Language
+
+                    //sql_cmd.CommandText = "SELECT count(Distinct Language) FROM books";
+                    //sql_cmd.ExecuteNonQuery();
+                    //Int32 countLanguage = Convert.ToInt32(sql_cmd.ExecuteScalar());
+
+                    DataTable dtable = new DataTable();
+                    GenreListView.Items.Clear();
+                    for (int i = 0; i < DTG.Rows.Count; i++)
+                    {
+                        DataRow drow = DTG.Rows[i];
+                        ListViewItem lvi = new ListViewItem { Content = drow.ToString() };
+                        GenreListView.Items.Add(new ListViewItem { Content = drow["Language"].ToString() });//+ " ("+ countLanguage+")" 
+                    }
+                }
+                sql_con.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //TODO: Alles toevoegen aan selectie in GenreListView
+        private void GenreListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (((ComboBoxItem)ComboGenreList.SelectedItem).Content.ToString().Equals("Genre"))
+            {
+                string filterExp = "GenreId = BookId AND Genre = '" + ((ListViewItem)GenreListView.SelectedItem).Content.ToString() + "'";
+                string sortExp = "Title";
+                DataRow[] drarray;
+                drarray = DT.Select(filterExp, sortExp, DataViewRowState.CurrentRows);
+                MainListView.Items.Clear();
+                for (int i = 0; i < drarray.Length; i++)
+                {
+                    MainListView.Items.Add(drarray[i]["Title"].ToString() + " - " + drarray[i]["Name"].ToString());
+                }
+            }
+            else if (((ComboBoxItem)ComboGenreList.SelectedItem).Content.ToString().Equals("Taal"))
+            {
+                string filterExp = "Language = '" + ((ListViewItem)GenreListView.SelectedItem).Content.ToString() + "'";
+                string sortExp = "Title";
+                DataRow[] drarray;
+                drarray = DT.Select(filterExp, sortExp, DataViewRowState.CurrentRows);
+                MainListView.Items.Clear();
+                for (int i = 0; i < drarray.Length; i++)
+                {
+                    MainListView.Items.Add(drarray[i]["Title"].ToString() + " - " + drarray[i]["Name"].ToString());
+                }
+            }
+        }
+
+
 
         //private void ExecuteQuery(string txtQuery)
         //{
